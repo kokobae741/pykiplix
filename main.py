28d5bd41a33991b668f8ff864dadb61f25d06496
kokobae741 <49124499+kokobae741@users.noreply.github.com>
2019-04-30 20:40:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ class Server:
         time.sleep(1)
         sys.stderr.write(self.config('Configuration'))
         time.sleep(2)
-        sys.stderr.write(self.log('========Inject Sukses======== \nReading Server:'))
+        sys.stderr.write(self.log('==BangKoko By:kumpul4semut== \nGunakan dengan bijak, matursuwun kumpul4semut:'))
 
     def run(self):
         ServerHandler.LogWindow(True)
